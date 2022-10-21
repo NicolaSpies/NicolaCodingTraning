8235607dc2d5813b5e8dc645fe56fde169a5e65b
Jurie Spies <juriespies@gmail.com>
2022-10-21 05:58:32 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import "./styles.css";
-
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start here Nicola!</h2>
+      <h2>Start here Nicola!!</h2>
+      <div style={{ display: flex, height: 100, width: 100, color: green }} />
     </div>
   );
 }
